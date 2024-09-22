32d6c48e4ca53698469f2a06aeec216d599ec378
Saurabh Kumar <saurabhkumar22278@gmail.com>
2024-09-22 15:58:01 +0530

    mail Button

--- src/components/jobsDisc/JobDisc.jsx
+++ src/components/jobsDisc/JobDisc.jsx
@@ -1,3 +1,80 @@
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+
+// const JobDisc = () => {
+//   const { jobId } = useParams();
+//   const [job, setJob] = useState(null);
+
+//   useEffect(() => {
+//     fetch(
+//       "https://raw.githubusercontent.com/Shubham-bit-hash/Assets/main/job_postings.json"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const jobData = data.find((job) => job.jobId === jobId);
+//         setJob(jobData);
+//       })
+//       .catch((error) => console.error("Error fetching job:", error));
+//   }, [jobId]);
+
+//   if (!job) return <div>Loading...</div>;
+
+//   return (
+//     <section>
+//       <div className=" mx-36 mt-10 xs:mx-4 xs:mt-28 md:mx-20 md:mt-32 lg:mx-36">
+//         <h1 className="font-semibold text-xl">Job Title: {job.title}</h1>
+//         {/* <p>Company: {job.company}</p> */}
+//         <div className="my-5">
+//           <div>
+//             <span className="font-medium">Company:</span> {job.company}
+//           </div>
+//           <div>
+//             <span className="font-medium">Location:</span> {job.location}
+//           </div>
+//           <div>
+//             <span className="font-medium">Type:</span> {job.type}
+//           </div>
+//         </div>
+//         <h3 className="font-semibold text-lg">About Us:</h3>
+//         <p className="my-4 text-gray-700">{job.about_us}</p>
+//         <h3 className="font-semibold text-lg">Role Overview:</h3>
+//         <p className="my-4 text-gray-700">{job.role_overview}</p>
+
+//         <h3 className="font-semibold text-lg">Key Skills:</h3>
+//         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
+//           {job.skills.map((skill,index) => {
+//             return (
+//               <li key={index}>
+//                 <span className="font-semibold text-semibold">{skill}</span>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//         <h3 className="font-semibold text-lg">Bouns Skills:</h3>
+//         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
+//           {job.bonus_skills.map((bouns,index) => {
+//             return <li key={index}>{bouns}</li>;
+//           })}
+//         </ul>
+//         <h3 className="font-semibold text-lg">Perks:</h3>
+//         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
+//           {job.perks.map((perk,index) => {
+//             return <li key={index}>{perk}</li>;
+//           })}
+//         </ul>
+//         <div className="w-full m-auto flex justify-center items-center">
+//           <button className="bg-[#93C47D] py-2 px-6 text-2xl font-bold  my-5 mt-4 hover:bg-[#b3e29d] duration-500">
+//             Apply
+//           </button>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default JobDisc;
+
+
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -19,11 +96,24 @@ const JobDisc = () => {
 
   if (!job) return <div>Loading...</div>;
 
+  const ButtonMailto = ({ mailto, label }) => {
+    return (
+      <a
+        href={mailto}
+        onClick={(e) => {
+          window.location.href = mailto;
+          e.preventDefault();
+        }}
+      >
+        {label}
+      </a>
+    );
+  };
+
   return (
     <section>
       <div className=" mx-36 mt-10 xs:mx-4 xs:mt-28 md:mx-20 md:mt-32 lg:mx-36">
         <h1 className="font-semibold text-xl">Job Title: {job.title}</h1>
-        {/* <p>Company: {job.company}</p> */}
         <div className="my-5">
           <div>
             <span className="font-medium">Company:</span> {job.company}
@@ -42,30 +132,32 @@ const JobDisc = () => {
 
         <h3 className="font-semibold text-lg">Key Skills:</h3>
         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
-          {job.skills.map((skill,index) => {
-            return (
-              <li key={index}>
-                <span className="font-semibold text-semibold">{skill}</span>
-              </li>
-            );
-          })}
+          {job.skills.map((skill, index) => (
+            <li key={index}>
+              <span className="font-semibold text-semibold">{skill}</span>
+            </li>
+          ))}
         </ul>
-        <h3 className="font-semibold text-lg">Bouns Skills:</h3>
+        <h3 className="font-semibold text-lg">Bonus Skills:</h3>
         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
-          {job.bonus_skills.map((bouns,index) => {
-            return <li key={index}>{bouns}</li>;
-          })}
+          {job.bonus_skills.map((bonus, index) => (
+            <li key={index}>{bonus}</li>
+          ))}
         </ul>
         <h3 className="font-semibold text-lg">Perks:</h3>
         <ul className="my-4 list-disc px-16 text-gray-700 xs:px-8 md:pl-16">
-          {job.perks.map((perk,index) => {
-            return <li key={index}>{perk}</li>;
-          })}
+          {job.perks.map((perk, index) => (
+            <li key={index}>{perk}</li>
+          ))}
         </ul>
-        <div className="w-full m-auto flex justify-center items-center">
-          <button className="bg-[#93C47D] py-2 px-6 text-2xl font-bold  my-5 mt-4 hover:bg-[#b3e29d] duration-500">
-            Apply
-          </button>
+        <div className="w-full m-auto flex justify-center items-center ">
+          <div  className="bg-[#93C47D] text-gray-700 py-2 px-6 text-2xl font-bold  my-5 mt-4 hover:bg-[#b3e29d] duration-500">
+
+          <ButtonMailto
+            label="Apply"
+            mailto={`mailto:hr@gmail.com?subject=Applying for ${job.title}`}
+            />
+            </div>
         </div>
       </div>
     </section>
